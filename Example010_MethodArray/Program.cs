569d80de31b66bfe0ca47d8a1d95db13d9116c56
Sherzod <asssher.uz@gmail.com>
2023-03-07 13:31:40 +0500

    Added Example011

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 11, 21, 31, 41, 19, 15, 61, 17, 18, 19 };
+int n = array.Length;
+int find = 19;
+int index = 0;
+while (index < n)
+{
+	if(array[index] == find)
+	{
+		Console.WriteLine(index);
+		break;
+	}
+	// index = index +1;
+		index++;
+}
