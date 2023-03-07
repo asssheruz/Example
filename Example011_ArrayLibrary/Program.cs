043322cd018664dd0846e7b8c943112113e22cb6
Sherzod <asssher.uz@gmail.com>
2023-03-07 18:27:50 +0500

    Example011_ArrayLibrary

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -39,10 +39,10 @@ int IndexOf(int[] collection, int find)
 int[] array = new int[10];
 
 FillArray(array);
-// array[4] = 4;
-// array[6] = 4;
+array[4] = 4;
+array[6] = 4;
 PrintArray(array);
 Console.WriteLine();
 
-int pos = IndexOf(array, 444);
+int pos = IndexOf(array, 44);
 Console.WriteLine(pos);
