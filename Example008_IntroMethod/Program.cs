1bc8d74810d9587fbd8afe8e48cdca6d6766fa6a
Sherzod <asssher.uz@gmail.com>
2023-03-07 18:00:35 +0500

    Example008_IntroMethod

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -3,12 +3,12 @@
 	int result = arg1;
 	if (arg2 > result) result = arg2;
 	if (arg3 > result) result = arg3;
-	return result;		
+	return result;
 }
 int a1 = 1544;
 int b1 = 21;
 int c1 = 39;
-int a2 = 12;
+int a2 = 1255;
 int b2 = 23;
 int c2 = 33;
 int a3 = 13;
@@ -19,11 +19,14 @@ int c3 = 313;
 // int max2 = Max(a2, b2, c2);
 // int max3 = Max(a3, b3, c3);
 // int max = Max(max1, max2, max3);
+
 // if(b1 > max) max = b1;
 // if(c1 > max) max = c1;
+
 // if (a2 > max) max = a2;
 // if (b2 > max) max = b2;
 // if (c2 > max) max = c2;
+
 // if (a3 > max) max = a3;
 // if (b3 > max) max = b3;
 // if (c3 > max) max = c3;
