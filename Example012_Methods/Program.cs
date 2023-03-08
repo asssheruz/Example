d8cbe2d4cab5dfc939810a92f16e5740c168b59c
Sherzod <asssher.uz@gmail.com>
2023-03-09 01:33:43 +0500

    Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -3,7 +3,7 @@ void Method1()
 {
 	Console.WriteLine("Автор ...");
 }
-Method1();
+// Method1();
 
 
 // Вид 2
@@ -11,7 +11,7 @@ void Method2(string msg)
 {
 	Console.WriteLine(msg);
 }
-Method2(msg: "Текст сообщения");
+// Method2(msg: "Текст сообщения");
 
 void Method21(string msg, int count)
 {
@@ -23,7 +23,7 @@ void Method21(string msg, int count)
 	}
 }
 // Method21(msg: "Текст", count: 4);
-Method21(count: 4, msg: "Новый текст");
+// Method21(count: 4, msg: "Новый текст");
 
 
 // Вид 3
@@ -33,23 +33,116 @@ int Method3()
 	return DateTime.Now.Year;
 }
 int year = Method3();
-Console.WriteLine(year);
+// Console.WriteLine(year);
 
 
 // Вид 4
 
 string Method4(int count, string text)
 {
-	int i = 0;
-	string result = String.Empty;
-
-	while (i < count)
-	{
-		result = result + text;
-		i++;
-	}
+		string result = String.Empty;
+	for (int i = 0; i < count; i++)
+		{
+			result = result + text;
+		}
 	return result;
 }
 
 string res = Method4(10, "Z ");
-Console.WriteLine(res);
+// Console.WriteLine(res);
+
+
+// for (int i = 2; i <= 10; i++)
+// {
+// 	for (int j = 2; j <= 10; j++)
+// 	{
+// 		Console.WriteLine($"{i} x {j} = {i * j}");
+// 	}
+// 		Console.WriteLine();
+// }
+
+
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что," 
+							+ "ежали бы вас послали вместо нашего милого Винценгероде,"
+							+ "вы бы взяли приступом согласие пусского короля. "
+							+ "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string = "qwerty"
+// 						012
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+	string result = String.Empty;
+
+	int length = text.Length;
+	for (int i = 0; i < length; i++)
+	{
+		if(text[i] == oldValue) result = result + $"{newValue}";
+		else result = result + $"{text[i]}";
+	}
+	return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(text, 'к', 'K');
+// Console.WriteLine(newText);
+
+int[] arr = { 8, 5, 4, 3, 2, 6, 7, 1, 9};
+
+void PrintArray(int[] array)
+{
+	int count = array.Length;
+
+	for (int i = 0; i < count; i++)
+	{
+		Console.Write($"{array[i]} ");
+	}
+	Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+		for (int i = 0; i < array.Length - 1; i++)
+	{
+		int minPosition = i;
+		for (int j = i + 1; j < array.Length; j++)
+		{
+			if(array[j] < array[minPosition]) minPosition = j;
+		}
+		
+		int temporary = array[i];
+		array[i] = array[minPosition];
+		array[minPosition] = temporary;
+	}
+}
+
+void SelectionSortMax(int[] array)
+{
+	for (int i = 0; i < array.Length - 1; i++)
+	{
+		int maxPosition = i;
+		for (int j = i + 1; j < array.Length; j++)
+		{
+			if (array[j] > array[maxPosition]) maxPosition = j;
+		}
+
+		int temporary = array[i];
+		array[i] = array[maxPosition];
+		array[maxPosition] = temporary;
+	}
+}
+
+
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+SelectionSortMax(arr);
+
+PrintArray(arr);
